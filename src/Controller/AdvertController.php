f3b8f5c04969e69d4ec5273ad29dcd2219dee896
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-04-28 23:47:24 +0300

    #13 Move method advert() from HomeController to AdvertController

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Advert;
+use App\Entity\Offer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class AdvertController extends AbstractController
 {
@@ -14,6 +19,47 @@ class AdvertController extends AbstractController
     {
         return $this->render('advert/index.html.twig', [
             'controller_name' => 'AdvertController',
+        ]);
+    }
+
+    /**
+     * @Route("/advert/{id}", name="advert", requirements={"id"="\d+"})
+     * @ParamConverter("advert", class="App:Advert")
+     * @param Advert $advert
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function advert(
+        Advert $advert,
+        Request $request
+    ) {
+
+        $offer = new Offer($advert);
+
+        $offerForm = $this->createFormBuilder($offer)
+            ->add('email')
+            ->add('text')
+            ->add('save', SubmitType::class, ['label' => 'Siųsti'])
+            ->getForm();
+
+        $offerForm->handleRequest($request);
+
+        if ($offerForm->isSubmitted() && $offerForm->isValid()) {
+            $offer = $offerForm->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($offer);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Siūlymas išsaugotas');
+
+            return $this->redirect($request->getUri());
+        }
+
+        return $this->render('advert/advert.html.twig', [
+            'advert' => $advert,
+            'offerForm' => $offerForm->createView(),
         ]);
     }
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\Advert;
 use App\Entity\Category;
-use App\Entity\Offer;
 use App\Repository\AdvertRepository;
 use App\Repository\CategoryRepository;
 use App\SearchObjects\Filters;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class HomeController extends AbstractController
 {
@@ -28,7 +24,6 @@ class HomeController extends AbstractController
      */
     public function index(Request $request, AdvertRepository $advertRepository, CategoryRepository $categoryRepository)
     {
-
         $page = $request->query->get('page') ? $request->query->get('page') : 1;
 
         $filters = new Filters();
@@ -54,48 +49,6 @@ class HomeController extends AbstractController
             'filteredAdverts' => $filteredAdverts->getIterator(),
             'page' => $page,
             'toggleQueryStrings' => $this->buildToggleQueryStrings($availableCategories, $selectedCategories)
-        ]);
-    }
-
-
-    /**
-     * @Route("/advert/{id}", name="advert", requirements={"id"="\d+"})
-     * @ParamConverter("advert", class="App:Advert")
-     * @param Advert $advert
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Exception
-     */
-    public function advert(
-        Advert $advert,
-        Request $request
-    ) {
-
-        $offer = new Offer($advert);
-
-        $offerForm = $this->createFormBuilder($offer)
-            ->add('email')
-            ->add('text')
-            ->add('save', SubmitType::class, ['label' => 'Siųsti'])
-            ->getForm();
-
-        $offerForm->handleRequest($request);
-
-        if ($offerForm->isSubmitted() && $offerForm->isValid()) {
-            $offer = $offerForm->getData();
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($offer);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Siūlymas išsaugotas');
-
-            return $this->redirect($request->getUri());
-        }
-
-        return $this->render('home/advert.html.twig', [
-            'advert' => $advert,
-            'offerForm' => $offerForm->createView(),
         ]);
     }
 
