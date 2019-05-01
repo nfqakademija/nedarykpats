b1941c5bf02f0ae49ee4849ad68f7d134c787361
Martyna Bikulciute <martyna.bikulciute@gmail.com>
2019-05-01 15:10:29 +0300

    #10 Add category input in form of new advert

--- assets/js/react.jsx
+++ assets/js/react.jsx
@@ -46,4 +46,83 @@ if (typeof usingReactApp !== "undefined") {
     })();
 
     FloatLabel.init();
+
+}
+
+if (typeof usingReactApp !== "undefined") {
+    const toogleCategories = (() => {
+
+        let categories = [];
+
+        const updateList = () => {
+            const categoriesList = document.getElementById('CategoriesList');
+            categoriesList.innerHTML = categories;
+        };
+
+        const addCategory = (element) => {
+            const advertCategories = document.getElementById('AdvertCategories');
+
+            advertCategories.innerHTML +=
+                '<span class="Category Category--filter is-newCategory">' +
+                '<span class="Filter-title">' +
+                element +
+                '</span>' +
+                '<span class="Filter-icon">' +
+                '<i class="fas fa-times"></i>' +
+                '</span>' +
+                '</span>';
+            categories.push(element);
+            updateList();
+        };
+
+        const removeCategory = (element) => {
+            categories = categories.filter(item => item !== element);
+            updateList();
+        };
+
+        const selectCategories = (element) => {
+            element.addEventListener("change", function() {
+                const advertValue = element.options[element.selectedIndex].text;
+                const isInArray = categories.includes(advertValue);
+
+                (isInArray) ? removeCategory(advertValue) : addCategory(advertValue);
+            });
+        };
+
+        const removeTags = (element) => {
+            const tag = element.childNodes[0];
+            const isClass = tag.classList.contains('Filter-title');
+
+            if (isClass) {
+                removeCategory(tag.textContent);
+                element.style.opacity = '0';
+                setTimeout(function(){
+                    element.parentNode.removeChild(element);
+                    }, 500);
+            }
+        };
+
+        const init = () => {
+            const advertList = document.getElementById('AdvertList');
+            const tagParent = document.getElementById('AdvertCategories');
+
+            if (advertList) {
+                selectCategories(advertList);
+            }
+
+            if (tagParent) {
+                tagParent.addEventListener("click",function(e) {
+                    const tag = e.path[1];
+                    removeTags(tag);
+                });
+            }
+        };
+
+        return {
+            init: init
+        };
+
+    })();
+
+    toogleCategories.init();
 }
