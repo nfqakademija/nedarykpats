940b9af46dd70d8056d76517162283258c2183bb
Martyna Bikulciute <martyna.bikulciute@gmail.com>
2019-05-11 16:28:33 +0300

    Looking beyond the embers of bridges glowing behind us

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AddCategories from './components/createForm/AddCategories.jsx';
 import AlertDialog from './components/dialogs/AlertDialog';
+import LayoutMasonry from './components/layout/LayoutMasonry';
 
-require('bootstrap');
+// require('bootstrap');
 require('../css/app.scss');
+
+// const layoutItems = document.querySelectorAll('.Ad');
+// const layoutRoot = document.querySelectorAll('#Ads');
+// ReactDOM.render(
+//     <LayoutMasonry
+//         layoutRoot = {layoutRoot}
+//         layoutItems = {layoutItems}/>,
+//     document.getElementById("Ads")
+// );
 
 const categories = document.getElementById('AdvertCategories');
 if (categories) {
@@ -24,16 +34,3 @@ let categoryContainer = document.getElementsByClassName("css-1pcexqc-container")
 for(let i = 0; i < categoryContainer.length; i++) {
     categoryContainer[i].className += " Form-select";
 };
-
-const alertDialog = document.getElementById("Alert");
-
-if (alertDialog) {
-    try {
-        ReactDOM.render(
-        <AlertDialog {...(alertDialog.dataset)}/>,
-        alertDialog
-    );
-    } catch (error) {
-        console.error(error);
-    }
-}
