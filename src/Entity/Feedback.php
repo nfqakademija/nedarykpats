865e4580190d5eca335db6b735dd03a0ab2855b9
vgumonis <vilius.gumonis@coingate.com>
2019-05-14 20:40:45 +0300

    #15 Add Feedback entity

---
+++ src/Entity/Feedback.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FeedbackRepository")
+ */
+class Feedback
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Advert", inversedBy="feedback", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $advert;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="feedback")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $receivingUser;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $score;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $message;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Advert|null
+     */
+    public function getAdvert(): ?Advert
+    {
+        return $this->advert;
+    }
+
+    /**
+     * @param Advert $advert
+     * @return Feedback
+     */
+    public function setAdvert(Advert $advert): self
+    {
+        $this->advert = $advert;
+
+        return $this;
+    }
+
+    /**
+     * @return User|null
+     */
+    public function getReceivingUser(): ?User
+    {
+        return $this->receivingUser;
+    }
+
+    /**
+     * @param User|null $receivingUser
+     * @return Feedback
+     */
+    public function setReceivingUser(?User $receivingUser): self
+    {
+        $this->receivingUser = $receivingUser;
+
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    /**
+     * @param int $score
+     * @return Feedback
+     */
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string|null $message
+     * @return Feedback
+     */
+    public function setMessage(?string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
