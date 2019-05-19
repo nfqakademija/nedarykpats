5a35f28599d478d4c5b4889f7c0d42134f6b5e0a
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-05-20 01:58:07 +0300

    Add email notice funcitonality to ProfilePasswordChangeHandler::handle(). Alter profile.html.twig to display correct empty profile.

--- src/Handler/ProfilePasswordChangeHandler.php
+++ src/Handler/ProfilePasswordChangeHandler.php
@@ -5,6 +5,7 @@ namespace App\Handler;
 
 use App\DTO\ProfilePasswordDTO;
 use App\Entity\User;
+use App\Service\TokenGeneratorService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -28,23 +29,40 @@ class ProfilePasswordChangeHandler
     private $passwordEncoder;
 
     /**
+     * @var TokenGeneratorService
+     */
+    private $tokenGeneratorService;
+
+    /**
+     * @var EmailHandler
+     */
+    private $emailHandler;
+
+    /**
      * @param EntityManagerInterface $entityManager
      * @param TokenStorageInterface $tokenStorage
      * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param TokenGeneratorService $tokenGeneratorService
+     * @param EmailHandler $emailHandler
      */
     public function __construct(
         EntityManagerInterface $entityManager,
         TokenStorageInterface $tokenStorage,
-        UserPasswordEncoderInterface $passwordEncoder
+        UserPasswordEncoderInterface $passwordEncoder,
+        TokenGeneratorService $tokenGeneratorService,
+        EmailHandler $emailHandler
     ) {
         $this->entityManager = $entityManager;
         $this->tokenStorage = $tokenStorage;
         $this->passwordEncoder = $passwordEncoder;
+        $this->tokenGeneratorService = $tokenGeneratorService;
+        $this->emailHandler = $emailHandler;
     }
 
     /**
      * @param ProfilePasswordDTO $profilePasswordDTO
      * @return void
+     * @throws \Exception
      */
     public function handle(ProfilePasswordDTO $profilePasswordDTO): void
     {
@@ -53,6 +71,12 @@ class ProfilePasswordChangeHandler
 
         $newPassword = $this->passwordEncoder->encodePassword($user, $profilePasswordDTO->getNewPassword());
         $user->setPassword($newPassword);
+
+        $this->emailHandler->sendEmailPasswordWasChanged(
+            $user->getEmail(),
+            $this->tokenGeneratorService->generate($user, null, null)->getHash()
+        );
+
         $this->entityManager->flush();
     }
 }
