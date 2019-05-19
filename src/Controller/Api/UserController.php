de5d7550afe8f9e979d3e7a8321a673e927b2370
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-05-20 02:26:08 +0300

    Refactor FeedbackController and UserController namespaces

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace App\Controller\api;
+namespace App\Controller\Api;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UserController
+class UserController extends AbstractController
 {
 
     /**
