e4b2a360c1b76d7be429025de343fb50da2a2cba
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-05-20 01:59:19 +0300

    #15 Add user check in FeedbackController::newFeedback(). Add restriction to FeedbackController::newFeedback() only for logged in user.

--- src/Controller/FeedbackController.php
+++ src/Controller/FeedbackController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controller;
 
-use App\DTO\FeedbackFormDTO;
+use App\Entity\Advert;
 use App\Form\FeedbackFormType;
 use App\Handler\FeedbackCreationHandler;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,40 +15,40 @@ class FeedbackController extends AbstractController
 {
 
     /**
-     * @Route("api/feedbacks", name="feedbacks", requirements={"POST"})
+     * @Route("api/feedback", name="feedback", requirements={"POST"})
      * @param Request $request
      * @param FeedbackCreationHandler $feedbackCreationHandler
      * @return Response
-     * @throws \Exception
+     * @throws Exception
      */
     public function newFeedback(Request $request, FeedbackCreationHandler $feedbackCreationHandler): Response
     {
-        $feedbackForm = $this->createForm(FeedbackFormType::class);
         $feedbackDTOData = json_decode($request->getContent(), true);
+        $advert = $this->getDoctrine()->getRepository(Advert::class)->find($feedbackDTOData['advert']);
+
+        if ($advert->getUser() !== $this->getUser()) {
+            return new Response(
+                json_encode(['success' => false,]),
+                Response::HTTP_METHOD_NOT_ALLOWED
+            );
+        }
+
+        $feedbackForm = $this->createForm(FeedbackFormType::class);
         $feedbackForm->submit($feedbackDTOData);
 
         if ($feedbackForm->isSubmitted() && $feedbackForm->isValid()) {
             $feedbackDTO = $feedbackForm->getData();
-            //TODO: prideti try catch,meta 500 jei negeri duomenys arba invalidi forma
-            //TODO: erroras,jei dublouojasi ir jei toks vartotojas neegzistuoja
             try {
                 $result = $feedbackCreationHandler->handle($feedbackDTO);
-                if ($result) {
-                    return (new Response(json_encode(['success' => true])));
-                }
-            } catch (\Exception $e) {
-                return (new Response(
-                    json_encode(['success' => false, 'Error' => $e->getMessage()]),
+                return new Response(json_encode(['success' => $result]));
+            } catch (Exception $e) {
+                return new Response(
+                    json_encode(['success' => false,]),
                     Response::HTTP_BAD_REQUEST
-                ));
+                );
             }
-
-            return (new Response(
-                json_encode(['success' => false, 'Error' => 'Empty data']),
-                Response::HTTP_BAD_REQUEST
-            ));
         }
 
-        return (new Response(json_encode(['success' => false, 'Error' => 'Invalid form']), Response::HTTP_BAD_REQUEST));
+        return new Response(json_encode(['success' => false,]));
     }
 }
