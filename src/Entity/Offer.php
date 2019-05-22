26d680d7431190d4568ac37fe4dc26353d67c819
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-05-23 00:45:14 +0300

    Add unique index to offer table

--- src/Entity/Offer.php
+++ src/Entity/Offer.php
@@ -7,6 +7,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\OfferRepository")
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="advert_user_idx", columns={"advert_id", "user_id"})})
  */
 class Offer
 {
