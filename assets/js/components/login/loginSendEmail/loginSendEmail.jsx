70d843ff8c5103c718fef9f813fec372287eca2f
Martyna Bikulciute <martyna.bikulciute@gmail.com>
2019-05-23 03:04:34 +0300

    #13 Add email provider link in login form

--- assets/js/components/login/loginSendEmail/loginSendEmail.jsx
+++ assets/js/components/login/loginSendEmail/loginSendEmail.jsx
@@ -3,8 +3,32 @@ import LoginWasSentImage from '../loginImages/loginWasSentImage.jsx';
 
 class LoginSendEmail extends Component{
 
+    constructor() {
+        super();
+        this.state = {
+            providerLink: 'www.google.com'
+        };
+    }
+
+    findEmailProvider = () => {
+        const { email } = this.props.values;
+        const domain = email.substring(email.lastIndexOf("@") +1);
+        const domainArray = ['google.com', 'yahoo.com', 'mailinator.com'];
+
+        if (domain in domainArray) {
+            this.state.providerLink = 'www.' + domain;
+        }
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        window.location.href = this.state.providerLink;
+    }
+
     render(){
         const { email } = this.props.values;
+
+        this.findEmailProvider();
 
         return(
             <div>
@@ -13,8 +37,18 @@ class LoginSendEmail extends Component{
                 <h3 className="u-text-center u-margin-bottom">Išsiuntėme  prisijungimo nuorodą el. paštu </h3>
                 <p className="u-text-center">{email}</p>
 
-                <div className="u-margin-top-bottom u-align-center">
-                    <a href="#" className="Button Button--blue"> Gerai </a>
+                <div className="u-margin-top u-align-center">
+                    <button
+                       className="Button Button--blue"
+                       onClick={this.onSubmit}
+                    >
+                        Eiti į el. paštą
+                    </button>
+                </div>
+
+                <div className="u-align-center">
+                    <a href="/"
+                       className="Button Button--empty"> Pagrindinis </a>
                 </div>
             </div>
         )
