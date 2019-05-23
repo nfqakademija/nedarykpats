5f610df2b389a4b5e094231f6c7cca4320cfb2fe
Martyna Bikulciute <martyna.bikulciute@gmail.com>
2019-05-23 13:16:41 +0300

    #13 Add email validation

--- assets/js/components/login/loginEmail/loginEmail.jsx
+++ assets/js/components/login/loginEmail/loginEmail.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import Avatar from '../loginImages/loginAvatar.jsx';
+// import CreateFormAnimation from '../../../react';
 
 class LoginEmail extends Component{
 
+    constructor() {
+        super();
+        this.state = {
+            errors: {},
+        };
+    }
+
+    validateForm() {
+        let errors = {};
+        let formIsValid = true;
+        const { email } = this.props.values;
+        const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        let errorField = document.getElementById('Form-errors');
+
+        if (!pattern.test(email)) {
+            formIsValid = false;
+            errors["emailid"] = "Įveskite el. pašto adresą";
+            errorField.classList.remove('u-display-none');
+        }
+
+        this.setState({
+            errors: errors
+        });
+        return formIsValid;
+    }
+
     saveAndContinue = e => {
-        e.preventDefault();
-        this.props.nextStep();
+        if (this.validateForm()) {
+            e.preventDefault();
+            this.props.nextStep();
+        };
     };
 
     _handleKeyDown = e => {
@@ -28,11 +57,14 @@ class LoginEmail extends Component{
                     <input id="inputEmail"
                            type="email"
                            name="email"
+                           defaultValue={values.firstName}
                            onChange={this.props.handleChange('email')}
                            onKeyDown={this._handleKeyDown}
-                           defaultValue={values.firstName}
                            required
                     />
+                    <div id="Form-errors" className="Form-errors u-margin-bottom u-display-none">
+                        <li>{this.state.errors.emailid}</li>
+                    </div>
                 </div>
 
                 <div className="u-align-center">
