3c1f1d321dafbafd4f154ef50c64fbb8ea4b2a00
Aurimas Vilys <aurimas.vilys.96@gmail.com>
2019-05-25 17:32:19 +0300

    Update ImageUploadHandler annotations. Add ImageRemovalHandler. Update ImageGalleryFormDTO and ImageGalleryFromType

--- src/DTO/ImageGalleryFormDTO.php
+++ src/DTO/ImageGalleryFormDTO.php
@@ -13,10 +13,14 @@ class ImageGalleryFormDTO
      * @var File|null
      * @Assert\File(
      *     maxSize="1536k",
-     *     maxSizeMessage="Keliamas failas yra per didelis - ({{ size }} {{ suffix }}).",
-     *     mimeTypes={"jpeg", "png"})
+     *     maxSizeMessage="Keliamas failas yra per didelis - ({{ size }} {{ suffix }}).")
      */
     private $imageFile;
+
+    /**
+     * @var bool|null
+     */
+    private $mainPicture;
 
     /**
      * @return File|null
@@ -33,6 +37,24 @@ class ImageGalleryFormDTO
     public function setImageFile(?File $imageFile): ImageGalleryFormDTO
     {
         $this->imageFile = $imageFile;
+        return $this;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function isMainPicture(): ?bool
+    {
+        return $this->mainPicture;
+    }
+
+    /**
+     * @param bool $mainPicture|null
+     * @return ImageGalleryFormDTO
+     */
+    public function setMainPicture(?bool $mainPicture): ImageGalleryFormDTO
+    {
+        $this->mainPicture = $mainPicture;
         return $this;
     }
 }
