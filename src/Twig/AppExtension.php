92c9e01611440c93440d412df867a35a36c7e67b
vgumonis <vilius.gumonis@coingate.com>
2019-05-25 16:32:33 +0300

    Well, I stand up next to a mountain and I chop it down with the edge of my hand

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,6 +2,7 @@
 
 namespace App\Twig;
 
+use App\Entity\User;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -13,7 +14,8 @@ class AppExtension extends AbstractExtension
         return [
             new TwigFunction('isFeedbackAvailable', [FeedbackRuntime::class, 'isFeedbackAvailable']),
             new TwigFunction('getDataForFeedback', [FeedbackRuntime::class, 'getDataForFeedback']),
-            new TwigFunction('UserHasNotSubmittedOffer', [FeedbackRuntime::class, 'UserHasNotSubmittedOffer'])
+            new TwigFunction('userHasNotSubmittedOffer', [UserRuntime::class, 'userHasNotSubmittedOffer']),
+            new TwigFunction('advertIsNotUsers', [UserRuntime::class, 'advertIsNotUsers'])
         ];
     }
 }
