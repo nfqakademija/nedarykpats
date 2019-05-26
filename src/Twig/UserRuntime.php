a891ab47cfdf080c15ad9d9ba0b0b9f6684b4e15
vgumonis <vilius.gumonis@coingate.com>
2019-05-26 21:38:28 +0300

    I'm a street walking cheetah with a heart full of napalm

--- src/Twig/UserRuntime.php
+++ src/Twig/UserRuntime.php
@@ -9,36 +9,6 @@ use Twig\Extension\RuntimeExtensionInterface;
 class UserRuntime implements RuntimeExtensionInterface
 {
     /**
-     * @param User $user
-     * @param Advert $advert
-     * @return bool
-     */
-    public function userHasNotSubmittedOffer(?User $user, Advert $advert): bool
-    {
-        foreach ($advert->getOffers() as $offer) {
-            if ($offer->getUser() === $user) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
-    /**
-     * @param User $user
-     * @param Advert $advert
-     * @return bool
-     */
-    public function advertIsNotUsers(?User $user, Advert $advert): bool
-    {
-        if ($advert->getUser() === $user) {
-            return false;
-        }
-        return true;
-    }
-
-
-    /**
      * @param User|null $user
      * @param User $profileOwner
      * @return bool
@@ -46,5 +16,26 @@ class UserRuntime implements RuntimeExtensionInterface
     public function profileIsAppUsers(?User $user, User $profileOwner): bool
     {
         return ($user === $profileOwner) ? true : false;
+    }
+
+
+    /**
+     * @param User|null $user
+     * @param Advert $advert
+     * @return bool
+     */
+    public function showOfferForm(?User $user, Advert $advert): bool
+    {
+        if ($advert->getUser() === $user) {
+            return false;
+        }
+
+        foreach ($advert->getOffers() as $offer) {
+            if ($offer->getUser() === $user) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
