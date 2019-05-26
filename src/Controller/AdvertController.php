3c7ff967b8920dd0de70102b0b70fe4714e5d411
vgumonis <vilius.gumonis@coingate.com>
2019-05-26 15:59:03 +0300

    When I was a child a had a fever my hands my hands felt just like two ballons

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Constant\Pagination;
 use App\Entity\Advert;
 use App\Entity\User;
 use App\Form\AdvertType;
@@ -10,6 +11,8 @@ use App\Handler\AdvertCreationHandler;
 use App\Handler\AdvertRemovalHandler;
 use App\Handler\OfferCreationHandler;
 use App\Repository\AdvertRepository;
+use App\Repository\CategoryRepository;
+use App\SearchObjects\Filters;
 use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -92,6 +95,53 @@ class AdvertController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/my-adverts", name="my_adverts")
+     * @param Request $request
+     * @param AdvertRepository $advertRepository
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    public function myAdvert(
+        Request $request,
+        AdvertRepository $advertRepository
+    ): Response {
+        $page = $this->getPageInput($request);
+        $user = $this->getUser();
+
+        $filters = new Filters();
+        $statuses = [];
+
+        if ($request->query->get('status') != null) {
+            $statuses = explode(',', $request->query->get('status'));
+        }
+
+        $filters->setAdvertStatuses($statuses);
+
+        $filteredAdverts = $advertRepository->findMyAdvertsByCategories(
+            $user,
+            $filters,
+            $page,
+            Pagination::ITEMS_PER_PAGE
+        );
+
+        $paginationPages = ceil($filteredAdverts->count() / Pagination::ITEMS_PER_PAGE);
+
+        if ($paginationPages > 0 && $page > $paginationPages) {
+            $page = $paginationPages;
+            $filteredAdverts = $advertRepository
+                ->findMyAdvertsByCategories($user, $filters, $page, Pagination::ITEMS_PER_PAGE);
+        }
+
+        return $this->render('advert/my_adverts.html.twig', [
+            'paginationPages' => $paginationPages,
+            'filteredAdverts' => $filteredAdverts->getIterator(),
+            'page' => $page,
+            'toggleStatus' => $this->buildStatusToggle($statuses),
+            "status" => $statuses
+        ]);
+    }
+
 
     /**
      * @Route("/advert/{id}/remove", name="advert_remove", requirements={"id"="\d+"})
@@ -112,5 +162,44 @@ class AdvertController extends AbstractController
             $this->addFlash('fail', 'Skelbimo pašalinti nepavyko');
             return $this->redirectToRoute('my_adverts');
         }
+    }
+
+    /**
+     * @param array $selectedStatuses
+     * @return array
+     */
+    private function buildStatusToggle(array $selectedStatuses): array
+    {
+        $status = [
+            AdvertRepository::ADVERT_STATE_CURRENT,
+            AdvertRepository::ADVERT_STATE_WITHOUT_FEEDBACK,
+            AdvertRepository::ADVERT_STATE_HAS_FEEDBACK
+        ];
+
+        $statusToggle = [];
+        foreach ($status as $state) {
+            if (in_array($state, $selectedStatuses)) {
+                $statusToggle[$state] = strtr(implode(',', $selectedStatuses), [$state => '']);
+            } else {
+                $statusToggle[$state] = implode(',', $selectedStatuses) . ',' . $state;
+            }
+
+            $statusToggle[$state] = trim($statusToggle[$state], ', ');
+            $statusToggle[$state] = strtr($statusToggle[$state], [",," => ',']);
+        }
+        return $statusToggle;
+    }
+
+    /**
+     * @param Request $request
+     * @return int
+     */
+    private function getPageInput(Request $request)
+    {
+        $pageInput = $request->query->get('page') ? $request->query->get('page') : 1;
+        $pageCastToInt = ctype_digit($pageInput) ? $pageInput : 1;
+        $page = $pageCastToInt > 0 ? $pageCastToInt : 1;
+
+        return $page;
     }
 }
