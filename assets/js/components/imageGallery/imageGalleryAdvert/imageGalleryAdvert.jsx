68930441fc603d3076cbde19fcfc48918c19443f
Martyna Bikulciute <martyna.bikulciute@gmail.com>
2019-05-30 13:48:02 +0300

    Add imageGallery in advert page

--- assets/js/components/imageGallery/imageGalleryAdvert/imageGalleryAdvert.jsx
+++ assets/js/components/imageGallery/imageGalleryAdvert/imageGalleryAdvert.jsx
@@ -1,22 +1,54 @@
 import React from 'react';
-import Gallery from 'react-photo-gallery';
-import Lightbox from 'react-images';
+import ImageGallery from 'react-image-gallery';
 import axios from 'axios';
 
-class ImageGalleryAdvert extends React.Component {
+// const PREFIX_URL = 'https://raw.githubusercontent.com/xiaolin/react-image-gallery/master/static/';
 
+class AdvertImages extends React.Component {
     constructor() {
-
         super();
         this.state = {
-            currentImage: 0,
-            photos: [],
-            advert: ''
+            showIndex: false,
+            showBullets: true,
+            infinite: true,
+            showThumbnails: true,
+            showNav: true,
+            isRTL: false,
+            slideDuration: 450,
+            slideInterval: 2000,
+            slideOnThumbnailOver: false,
+            thumbnailPosition: 'bottom',
+            photosJson: [],
+            photos: []
         };
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
-        this.gotoNext = this.gotoNext.bind(this);
-        this.gotoPrevious = this.gotoPrevious.bind(this);
+    }
+
+    _onImageClick(event) {
+        console.debug('clicked on image', event.target, 'at index', this._imageGallery.getCurrentIndex());
+    }
+
+    _handleInputChange(state, event) {
+        this.setState({[state]: event.target.value});
+    }
+
+    _handleCheckboxChange(state, event) {
+        this.setState({[state]: event.target.checked});
+    }
+
+    _handleThumbnailPositionChange(event) {
+        this.setState({thumbnailPosition: event.target.value});
+    }
+
+    formatImagesArray() {
+        const  { photosJson, photos } = this.state;
+        photosJson.forEach(function(element) {
+            photos.push({
+                original: element.src,
+                thumbnail: element.src,
+            });
+        });
+
+        return photos;
     }
 
     getImages = () => {
@@ -24,60 +56,45 @@ class ImageGalleryAdvert extends React.Component {
 
         this.setState( () => {
             axios.get('/api/public/gallery?advert=' + advert)
-            .then( response =>  {
-                console.log(response);
-                this.setState({
-                    photos: response.data
+                .then( response =>  {
+                    this.setState({
+                        photosJson: response.data
+                    });
+                })
+                .catch(function (error) {
+                    console.log(error);
                 });
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
         });
     };
 
-  componentDidMount() {
-      this.getImages();
-  }
+    componentDidMount() {
+        this.getImages();
+    }
 
-    openLightbox(event, obj) {
-        this.setState({
-            currentImage: obj.index,
-            lightboxIsOpen: true,
-        });
-    }
-    closeLightbox() {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious() {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext() {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
     render() {
         const { photos } = this.state;
+        this.state.photos = [].concat(this.formatImagesArray());
 
         return (
-            <div>
-                <Gallery photos={photos} onClick={this.openLightbox} />
-                <Lightbox images={photos}
-                          onClose={this.closeLightbox}
-                          onClickPrev={this.gotoPrevious}
-                          onClickNext={this.gotoNext}
-                          currentImage={this.state.currentImage}
-                          isOpen={this.state.lightboxIsOpen}
-                />
-            </div>
-        )
+            <ImageGallery
+                ref={i => this._imageGallery = i}
+                items={photos}
+                lazyLoad={false}
+                onClick={this._onImageClick.bind(this)}
+                infinite={this.state.infinite}
+                showBullets={this.state.showBullets}
+                showThumbnails={this.state.showThumbnails}
+                showIndex={this.state.showIndex}
+                showNav={this.state.showNav}
+                isRTL={this.state.isRTL}
+                thumbnailPosition={this.state.thumbnailPosition}
+                slideDuration={parseInt(this.state.slideDuration)}
+                slideInterval={parseInt(this.state.slideInterval)}
+                slideOnThumbnailOver={this.state.slideOnThumbnailOver}
+                additionalClass="app-image-gallery"
+            />
+        );
     }
 }
 
-export default ImageGalleryAdvert;
+export default AdvertImages;
